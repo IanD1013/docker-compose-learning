5a2bb7b1a0035a1e1d3b40a9d4dc7672645bd3ed
IanD1013 <ian.tung1013@gmail.com>
2025-05-14 09:50:40 +1200

    Create demo application - backend

---
+++ docker-dometrain/DockerCourseApi/DockerCourseApi/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyOrigin());
+
+app.MapGet("/podcasts", () => new List<string>
+{
+    "Podcast 1",
+    "Podcast 2",
+    "Podcast 3",
+    "Podcast 4",
+});
+
+app.Run();
