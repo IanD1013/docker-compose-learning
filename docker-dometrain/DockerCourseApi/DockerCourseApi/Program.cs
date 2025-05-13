b0ad1e79c90d6ec1ec9c8891fc2d141f1e38c9a6
IanD1013 <ian.tung1013@gmail.com>
2025-05-14 10:23:15 +1200

    Create demo application - add simple database interaction

--- docker-dometrain/DockerCourseApi/DockerCourseApi/Program.cs
+++ docker-dometrain/DockerCourseApi/DockerCourseApi/Program.cs
@@ -1,3 +1,6 @@
+using System.Data.SqlClient;
+using Dapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors();
@@ -6,12 +9,26 @@ var app = builder.Build();
 
 app.UseCors(x => x.AllowAnyOrigin());
 
-app.MapGet("/podcasts", () => new List<string>
+app.MapGet("/podcasts", async () =>
 {
-    "Podcast 1",
-    "Podcast 2",
-    "Podcast 3",
-    "Podcast 4",
+    var db = new SqlConnection("Server=tcp:localhost;Initial Catalog=podcasts;Persist Security Info=False;User ID=sa;Password=Dometrain#123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
+
+    return (await db.QueryAsync<Podcast>("SELECT * FROM Podcasts")).Select(x => x.Title);
+
+    // return new List<string>
+    // {
+    //     "Unhandled Exception Podcast",
+    //     "Developer Weekly Podcast",
+    //     "The Stack Overflow Podcast",
+    //     "The Hanselminutes Podcast",
+    //     "The .NET Rocks Podcast",
+    //     "The Azure Podcast",
+    //     "The AWS Podcast",
+    //     "The Rabbit Hole Podcast",
+    //     "The .NET Core Podcast",
+    // };
 });
 
 app.Run();
+
+record Podcast(Guid Id, string Title);
